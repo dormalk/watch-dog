28aac3c0df1e466421b361eb019a777af9ee6204
dormalk <dormalk@gmail.com>
2023-02-17 12:55:39 +0200

    configure firebase

---
+++ src/firebase.tsx
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDqNE3V7n35ZhmJmPp6rY2pbyI-FXGVeX0",
+  authDomain: "watchdog-7cb56.firebaseapp.com",
+  projectId: "watchdog-7cb56",
+  storageBucket: "watchdog-7cb56.appspot.com",
+  messagingSenderId: "200522450258",
+  appId: "1:200522450258:web:81656f4c102a4b28b3a68b"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
