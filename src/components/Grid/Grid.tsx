7027bdfba3fb4824308effcd88f0edb6b72016d4
dormalk <dormalk@gmail.com>
2023-02-17 20:59:14 +0200

    set hebrew adjusments

--- src/components/Grid/Grid.tsx
+++ src/components/Grid/Grid.tsx
@@ -2,17 +2,20 @@ import React from 'react';
 import Row from 'react-bootstrap/Row';
 import Container from 'react-bootstrap/Container';
 import CardInfo from '../CardInfo/CardInfo';
+import { Users } from '../../utils/mocks/users';
 
 function Grid() {
   return (
     <Container className='mb-5' style={{marginTop: '2rem'}}>
         <Row xs={1} className="g-4">
-            {Array.from({ length: 15 }).map((_, idx) => (
-                <CardInfo   avatar='assets/placeholder_325_160.svg'
-                            starts={5}
-                            title='Card Tets'
-                            body="This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer."/>
-            ))}
+          {
+            Users.map((user,idx) =>
+              <CardInfo   avatar='assets/placeholder_325_160.svg'
+              starts={5}
+              title={user.fullName}
+              body={user.description}/>
+            )
+          }
         </Row>
     </Container>
   );
